1619072073960a77d0aaa8fed97eb5531acdb8fc
AlexisCarreras <alexiscarreras@hotmail.com>
2021-06-04 15:47:11 -0300

    ultimo

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import React, { Fragment, useState } from 'react';
-import Burger from "./components/Burger.Js";
+
+//Importo el componente Burguer
+import Burger from "./components/Burger";
+
+//Importo el componente Cart
+import Cart from "./components/Cart";
+
+//Importo hoja de stilos
+import "./styles/styles.css";
+
+//Importo las imágenes
+import Salteña from "./images/Salteña.png";
+import Porteña from "./images/Porteña.png";
+import Chicken from "./images/Chicken.png";
+import Criolla from "./images/Criolla.png";
+import Patagonia from "./images/patagonia.png";
 
 function App() {
 
   //Estado de hamburguesas con listado de hamburguesas
-  const [burgers, setBurgers] = useState([
-    { id: 1, nombre: 'Hamburguesa Completa', precio: 250 },
-    { id: 2, nombre: 'Hamburguesa con Cheddar', precio: 180 },
-    { id: 3, nombre: 'Hamburguesa con Jamón y Queso', precio: 150 },
-    { id: 4, nombre: 'Hamburguesa con Bacon', precio: 200 },
-    { id: 5, nombre: 'Hamburguesa de Pollo Completa', precio: 240 },
+  useState([
+    { id: 5, image: Salteña, nombre: "SALTEÑA", precio: 450 },
+    { id: 2, image: Porteña, nombre: "PORTEÑA", precio: 380 },
+    { id: 3, image: Chicken, nombre: "CHICKEN", precio: 350 },
+    { id: 4, image: Criolla, nombre: "CRIOLLA", precio: 400 },
+    { id: 1, image: Patagonia, nombre: "PATAGONIA", precio: 440 },
   ]);
 
   //Estado del Carrito
@@ -17,16 +32,56 @@ function App() {
 
   return (
     <Fragment>
-      <h3>Burgers</h3>
-      {burgers.map((burger) => (
-         <Burger
-          key={burger.id}
-          burger={burger}
-          cart={cart}
-          setCart={setCart}
-          burgers={burgers}
-        />
-      ))}
+      {/* SECTION CONTENEDOR GLOBAL */}
+      <section className="container">
+
+        {/* SECTION DE LA CABECERA */}
+        <section className="container__header">
+
+          {/* BOTON DE LA CABECERA */}
+          <div className="container__header__menu">
+            <button className="container__header__menu--button"></button>
+          </div>
+
+          {/* TITULO DE LA CABECERA */}
+          <div className="container__header__tittle">
+            <h3 className="container__header__tittle--text">
+              HAMBURGO-RDO
+            </h3>
+          </div>
+
+          {/* CART DE LA CABECERA */}
+          <div className="container__header__cartDiv">
+            <Cart cart={cart} setCart={setCart} />
+          </div>
+        </section>
+
+        {/* SECTION CONTENEDOR PRINCIPAL */}
+        <section className="container__content">
+
+          <div className="container__content__tittle">
+            <h1>Aprovecha sólo por tiempo limitado combos 50% off!</h1>
+          </div>
+
+          <div className="container__content__listBurgers">
+            {burgers.map((burger) => (
+              <Burger
+                key={burger.id}
+                burger={burger}
+                cart={cart}
+                setCart={setCart}
+                burgers={burgers}
+              />
+            ))}
+          </div>
+        </section>
+
+        {/* SECTION FOOTER */}
+        <section className="container__footer">
+          <p>HAMBURGO-RDO 2020 - Todos los derechos reservados</p>          
+        </section>
+        
+      </section>
     </Fragment>
   );
 }
---
+++ src/components/Cart.js
@@ -0,0 +1,64 @@
+import React, { Fragment } from 'react';
+
+//Importo el componente burguer
+import Burger from './Burger';
+
+//Importo los estilos para el cart
+import "../styles/styles.css";
+
+const Cart = ({ cart, setCart }) => {
+    const viewCart = () => {
+        {
+            document.querySelector(
+                "div.container__header__cartDiv--list"
+            ).style.display = "none"
+                ? (document.querySelector(
+                    "div.container__header__cartDiv--list"
+                ).style.display = "flex")
+                : (document.querySelector(
+                    "div.container__header__cartDiv--list"
+                ).style.display = "none");
+        }
+    };
+
+    const closeDiv = () => {
+        document.querySelector(
+          "div.container__header__cartDiv--list"
+        ).style.display = "none";
+    };
+
+    return (
+        <Fragment>
+            <div className="container__header__cartDiv">
+                <p className="container__header__cartDiv--count">{cart.length}</p>
+                <button
+                    className="container__header__cartDiv--btnCart" 
+                    onClick={() => viewCart()}
+                ></button>
+            </div>
+
+            <div className="container__header__cartDiv--list">
+                <button
+                    className="container__content__burgers--btnClose"
+                    type="button"
+                    onClick={() => closeDiv()}>
+                    X
+                </button>
+                {cart.length === 0 ? (
+                    <p>Aún no hay nada...</p>
+                ) : (
+                        cart.map((burger) => (
+                            <Burger
+                                key={burger.id}
+                                burger={burger}
+                                cart={cart}
+                                setCart={setCart}
+                            />
+                        ))
+                    )}
+            </div>
+        </Fragment>
+    );
+};
+
+export default Cart;
